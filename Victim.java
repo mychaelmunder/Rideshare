efb1748b765585a2a69508430c5c86ec506c59e4
Roy Otamura <rotamura24@students.polytechnic.org>
2023-02-01 18:47:10 -0800

    fixed

--- Victim.java
+++ Victim.java
@@ -2,8 +2,9 @@ public class Victim{
     int destination;
     int location;
     public Victim(){
-        location = (int)Math.random()*31;
-        destination = (int)Math.random()*31;
+        location = (int)(Math.random()*31);
+        destination = (int)(Math.random()*31);
     }
+    public int getLocation(){return location;}
 
 }
