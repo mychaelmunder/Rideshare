618ddc11a564a34e340e116beb96867d859e8e3f
Michael <122400385+mychaelmunder@users.noreply.github.com>
2023-02-03 14:36:37 -0800

    Updated run

--- runChicago.java
+++ runChicago.java
@@ -1,5 +1,11 @@
 public class runChicago {
     public static void main(String[] args) {
         Chicago chicago = new Chicago(50,20);
+        Victim vic1 = new Victim();
+        Victim vic2 = new Victim();
+        Victim vic3 = new Victim();
+        Victim vic4 = new Victim();
+        Victim vic5 = new Victim();
+        Victim vic6 = new Victim();
     }
 }
