307740ad512764f0fdf1557c47bde9371312cc6a
Michael <122400385+mychaelmunder@users.noreply.github.com>
2023-02-08 09:19:15 -0800

    tester1

--- Tester1.java
+++ Tester1.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 public class Tester1 {
     public static void main(String[] args) {
-        Scanner scan1 = new Scanner(System.in);
-        System.out.print("Enter # of victims: ");
-        int numvic = scan1.nextInt();
-        Scanner scan2 = new Scanner(System.in);
-        System.out.print("Enter # of vans: ");
-        int numvan = scan2.nextInt();
 
-        Chicago chicago = new Chicago(numvic, numvan);
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Enter # of vans: ");
+        int numvan = scan1.nextInt();
+
+        Chicago chicago = new Chicago(50, numvan);
             for(int bala = 0; bala < numvan; bala++){
                 chicago.kidnappers[bala].move();
             }
