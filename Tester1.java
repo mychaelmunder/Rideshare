010e8b425089eac77adff86b9434f43725418748
Roy Otamura <rotamura24@students.polytechnic.org>
2023-02-08 08:55:28 -0800

    fixed

--- Tester1.java
+++ Tester1.java
@@ -4,12 +4,12 @@ public class Tester1 {
     public static void main(String[] args) {
         Scanner scan1 = new Scanner(System.in);
         System.out.print("Enter # of victims: ");
-        String numvic = scan1.nextLine();
+        int numvic = scan1.nextInt();
         Scanner scan2 = new Scanner(System.in);
         System.out.print("Enter # of vans: ");
-        String numvan = scan2.nextLine();
+        int numvan = scan2.nextInt();
 
-        Chicago chicago = new Chicago(int numvic, int numvan);
+        Chicago chicago = new Chicago(numvic, numvan);
             for(int bala = 0; bala < numvan; bala++){
                 chicago.kidnappers[bala].move();
             }
