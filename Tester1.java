fbe3fd9491ca3899826f46cb6a4249aea1463697
Michael <122400385+mychaelmunder@users.noreply.github.com>
2023-02-08 08:51:43 -0800

    tester updated

--- Tester1.java
+++ Tester1.java
@@ -1,10 +1,19 @@
+import java.util.*;
+
 public class Tester1 {
     public static void main(String[] args) {
-        Victim vic1 = new Victim();
-        WhiteVan van1 = new WhiteVan();
-        WhiteVan van2 = new WhiteVan();
-        System.out.print(vic1.destination);
-           System.out.println(vic1.location);
-        Chicago chicago = new Chicago(50,20);
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Enter # of victims: ");
+        String numvic = scan1.nextLine();
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("Enter # of vans: ");
+        String numvan = scan2.nextLine();
+
+        Chicago chicago = new Chicago(int numvic, int numvan);
+            for(int bala = 0, bala < numvan, bala++)
+                WhiteVan[i].move()
+
+
+        
     }
 }
