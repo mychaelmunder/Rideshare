723c8318dc30e4d742eb83d5fbd283f61167e495
Michael <122400385+mychaelmunder@users.noreply.github.com>
2023-02-09 09:43:10 -0800

    update

--- Victim.java
+++ Victim.java
@@ -1,20 +1,20 @@
-public class Victim{
+public class Victim{ //passenger class tracking start location, current loc, and goal destination
     int destination;
     int location;
     int start;
     boolean isKidnapped;
     boolean atDestination;
     public Victim(){
-        location = (int)(Math.random()*31);
-        destination = (int)(Math.random()*31);
-        isKidnapped = false;
-        atDestination = false;
-        while(location == destination){destination = (int)(Math.random()*31);}
+        location = (int)(Math.random()*31); //random var from 0-30
+        destination = (int)(Math.random()*31); // same as above
+        isKidnapped = false; // in van set to false
+        atDestination = false; // at destination set to false
+        while(location == destination){destination = (int)(Math.random()*31);} // failsafe for if loc = dest
         start = location;
     }
-    public int getLocation(){return location;}
+    public int getLocation(){return location;} // getters
     public int getDestination(){return destination;}
     public boolean isKidnapped(){return isKidnapped;}
-    public void gone(int newLocation){location = newLocation;}
-    public void welcomeHome(){atDestination = true;}
+    public void gone(int newLocation){location = newLocation;} // new loc replaced with current
+    public void welcomeHome(){atDestination = true;} // arrival 
 }
