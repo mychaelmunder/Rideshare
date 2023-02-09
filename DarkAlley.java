be8f636ea2d73bb1e9f1ab22f146d58d7c3cc41a
Michael <122400385+mychaelmunder@users.noreply.github.com>
2023-02-09 09:46:44 -0800

    update

--- DarkAlley.java
+++ DarkAlley.java
@@ -1,25 +1,25 @@
 import java.util.*;
-public class DarkAlley {
+public class DarkAlley { // stations class
     ArrayList<Victim> victims;
-    int address;
+    int address; // all stations addressed as an int
     public DarkAlley(int location){
         victims = new ArrayList<Victim>();
         address = location;
     }
-    public void addVictim(Victim DominicRosato){victims.add(DominicRosato);}
-    public void victimKidnapped(Victim DominicRosato){victims.remove(DominicRosato);}
-    public ArrayList<Victim> isntHome(){
+    public void addVictim(Victim DominicRosato){victims.add(DominicRosato);} // put a victim into station
+    public void victimKidnapped(Victim DominicRosato){victims.remove(DominicRosato);} // take victim out
+    public ArrayList<Victim> isntHome(){ // if the victim isnt at the destination begin moving
         ArrayList<Victim> somewhereTheyShouldntBe = new ArrayList<Victim>();
         for(Victim DominicRosato:victims){
             if(!DominicRosato.atDestination){somewhereTheyShouldntBe.add(DominicRosato);}
         }
-        return somewhereTheyShouldntBe;
+        return somewhereTheyShouldntBe; // ! destination
     }
-    public ArrayList<Victim> isHome(){
+    public ArrayList<Victim> isHome(){ // if victim is at destination add
         ArrayList<Victim> somewhereTheyShouldBe = new ArrayList<Victim>();
         for(Victim DominicRosato:victims){
             if(DominicRosato.atDestination){somewhereTheyShouldBe.add(DominicRosato);}
         }
-        return somewhereTheyShouldBe;
+        return somewhereTheyShouldBe; // destination
     }
 }
