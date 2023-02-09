a3eb873e3c5a9a002002cd0cf8acc04cc3ce8904
Michael <122400385+mychaelmunder@users.noreply.github.com>
2023-02-09 09:47:46 -0800

    udpate

--- Chicago.java
+++ Chicago.java
@@ -1,12 +1,12 @@
-public class Chicago{
-    static Victim innocents[];
+public class Chicago{ // street class
+    static Victim innocents[]; // puts in victims vans and alleys
     static WhiteVan kidnappers[];
     static DarkAlley theStreets[];
-    public Chicago(int numVictims, int numVans){
+    public Chicago(int numVictims, int numVans){ // for tester, allows us to input numvic and numvan easily
         innocents = new Victim[numVictims];
         kidnappers = new WhiteVan[numVans];
         theStreets = new DarkAlley[31];
-        for(int bala = 0;bala < theStreets.length;bala++){theStreets[bala] = new DarkAlley(bala);}
+        for(int bala = 0;bala < theStreets.length;bala++){theStreets[bala] = new DarkAlley(bala);} // loops through victims and vans adding them
         for(int bala = 0; bala < kidnappers.length;bala++){kidnappers[bala] = new WhiteVan();}
         for(int bala = 0; bala < innocents.length;bala++){
             innocents[bala] = new Victim();
